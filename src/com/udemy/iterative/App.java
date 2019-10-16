25fa8045811e74719cec74d29f19b6746ef4eaa4
Tony <tjiang@kanetix.ca>
2019-10-16 14:41:07 -0400

    Ch2-3 Iteration - Adding numbers

--- src/com/udemy/iterative/App.java
+++ src/com/udemy/iterative/App.java
@@ -4,6 +4,8 @@ public class App {
 	public static void main(String[] args) {
 		Algorithm algorithm = new Algorithm();
 		
-		System.out.println(algorithm.sumIterative(6));
+//		System.out.println(algorithm.sumIterative(6));
+		
+		System.out.println(algorithm.sumRecursive(7));
 	}
 }
