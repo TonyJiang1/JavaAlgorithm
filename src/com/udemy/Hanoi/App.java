9a79dd8e1f3061ca1abcd7f66634b652787d1a2b
Tony <tjiang@kanetix.ca>
2019-10-16 16:17:32 -0400

    Ch2-10 Hanoi problem

---
+++ src/com/udemy/Hanoi/App.java
@@ -0,0 +1,10 @@
+package com.udemy.Hanoi;
+
+public class App {
+	public static void main(String[] args) {
+		Algorithm algorithm = new Algorithm();
+		
+		algorithm.solveHanoi(3, 'a', 'b', 'c');
+	}
+
+}
