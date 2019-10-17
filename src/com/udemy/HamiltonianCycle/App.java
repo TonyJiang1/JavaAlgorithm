a9309ce1f17e7a431daa8e5826b62d6fc13361d9
Tony <tjiang@kanetix.ca>
2019-10-16 22:26:55 -0400

    Ch4-24 Hamiltonian cycle problem

---
+++ src/com/udemy/HamiltonianCycle/App.java
@@ -0,0 +1,17 @@
+package com.udemy.HamiltonianCycle;
+
+public class App {
+	public static void main(String[] args) {
+		int[][] matrix = {
+				{0, 1, 1, 1, 0, 0},
+				{1, 0, 1, 0, 1, 0},
+				{1, 1, 1, 1, 0, 1},
+				{1, 0, 1, 0, 0, 1},
+				{0, 1, 0, 0, 0, 1},
+				{0, 1, 1, 1, 1, 1}
+		};
+		HamiltonianCycle cycle = new HamiltonianCycle(matrix);
+		cycle.solve();
+	};
+}
+
